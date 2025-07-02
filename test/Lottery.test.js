ad4a76ecba2099f5aa9ba8e83856aa67feaa2997
Ness Bokobza <ness.bokobza@hyperguest.com>
2025-07-02 21:19:56 +0300

    refactor: enhance Lottery contract tests with additional assertions and comments

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -12,30 +12,36 @@ beforeEach(async () => {
 	accounts = await web3.eth.getAccounts()
 
 	lottery = await new web3.eth.Contract(JSON.parse(interface))
+	// Deploy the Lottery contract to our local test network.
 		.deploy({ data: bytecode })
 		.send({ from: accounts[0], gas: "1000000" })
 })
 
 describe("Lottery Contract", () => {
 	it("deploys a contract", () => {
+		// Check that the contract has a valid address on the blockchain.
 		assert.ok(lottery.options.address)
 	})
 
 	it("allows one account to enter", async () => {
+		// Call the 'enter' function in the Solidity contract.
 		await lottery.methods.enter().send({
 			from: accounts[0],
 			value: web3.utils.toWei("0.02", "ether"),
 		})
 
+		// Call the 'getPlayers' view function to see who has entered.
 		const players = await lottery.methods.getPlayers().call({
 			from: accounts[0],
 		})
 
+		// Verify that the player's address was added to the 'players' array.
 		assert.equal(accounts[0], players[0])
 		assert.equal(1, players.length)
 	})
 
 	it("allows multiple accounts to enter", async () => {
+		// Test that multiple accounts can call the 'enter' function.
 		await lottery.methods.enter().send({
 			from: accounts[0],
 			value: web3.utils.toWei("0.02", "ether"),
@@ -49,6 +55,7 @@ describe("Lottery Contract", () => {
 			value: web3.utils.toWei("0.02", "ether"),
 		})
 
+		// Verify all accounts are in the 'players' array.
 		const players = await lottery.methods.getPlayers().call({
 			from: accounts[0],
 		})
@@ -60,39 +67,48 @@ describe("Lottery Contract", () => {
 	})
 
 	it("requires a minimum amount of ether to enter", async () => {
+		// Test that the 'enter' function reverts if not enough Ether is sent.
 		try {
 			await lottery.methods.enter().send({
 				from: accounts[0],
 				value: 0,
 			})
-			assert(false)
+			assert(false) // If the transaction didn't revert, the test should fail.
 		} catch (err) {
-			assert(err)
+			assert(err) // Assert that an error (revert) occurred.
 		}
 	})
 
 	it("only manager can call pickWinner", async () => {
+		// Test that only the contract manager can call 'pickWinner'.
 		try {
 			await lottery.methods.pickWinner().send({
 				from: accounts[1],
 			})
-			assert(false)
+			assert(false) // If the transaction didn't revert, the test should fail.
 		} catch (err) {
-			assert(err)
+			assert(err) // Assert that an error (revert) occurred.
 		}
 	})
 
 	it("sends money to the winner and resets the players array", async () => {
+		// Enter one account into the lottery.
 		await lottery.methods.enter().send({
 			from: accounts[0],
 			value: web3.utils.toWei("2", "ether"),
 		})
 
+		// Get initial balance of the winner.
 		const initialBalance = await web3.eth.getBalance(accounts[0])
+		// Call 'pickWinner' from the manager account.
 		await lottery.methods.pickWinner().send({ from: accounts[0] })
+		// Get final balance of the winner.
 		const finalBalance = await web3.eth.getBalance(accounts[0])
+		// Calculate the difference (should be close to 2 ether, minus gas).
 		const difference = finalBalance - initialBalance
 
+		// Assert that the winner received most of the prize money.
 		assert(difference > web3.utils.toWei("1.8", "ether"))
+		// (Implicitly, the contract should also reset the players array after picking a winner.)
 	})
 })
